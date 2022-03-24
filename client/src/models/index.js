2994050447d8fd7e8a850256c75d8844eb62a10d
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2022-03-23 21:38:44 -0700

    mongoose connect

---
+++ client/src/models/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Mongo connected DB at:", process.env.MONGO_URI);
+  }
+);
+
+module.exports.Logo = require("./logos.js");
---
+++ client/src/models/logos.js
@@ -0,0 +1,11 @@
+const mongoose = requrie("mongoose");
+
+const logoSchema = new mongoose.Schema({
+  company: { type: String, required: true },
+  url: {
+    type: String,
+  },
+  hint: { type: String },
+});
+
+module.exports = mongoose.model("Logo", logoSchema);
