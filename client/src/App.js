ea3a113c8d848a889c33c4caf954707109f89ee7
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2022-03-23 19:46:07 -0700

    Revert "routes are working for all game, library, and home"

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,18 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Game from "./components/game";
-import Home from "./components/home";
-import Library from "./components/library";
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("/api")
-      .then(res => res.json())
-      .then(data => setData(data.message));
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
   }, []);
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/library" element={<Library />} />
-        </Routes>
-      </BrowserRouter>
-      <p>{data}</p>
+        <p>{data}</p>
     </div>
   );
 }
