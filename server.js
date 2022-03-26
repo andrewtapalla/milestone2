05fb8e5b201f6618f0bb4d32ca1c479f919f6cd5
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2022-03-26 11:39:05 -0700

    navbar & mongoose

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-require("dotenv").config();
+//Exoress Config
 const express = require("express");
+
 const app = express();
 
-const PORT = 3001;
+//Mongo Config
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Mongo connected DB at:", process.env.MONGO_URI);
+  }
+);
+
+//Controllers
+const logoController = require("./client/src/models/logos");
+app.use("/logos", logoController);
 
 app.get("/api", (req, res) => {
   res.json({
@@ -10,6 +25,6 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log("yellow world on " + PORT);
+app.listen(process.env.PORT, () => {
+  console.log("yellow world on " + process.env.PORT);
 });
