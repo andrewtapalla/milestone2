46c123cc193aebf720281f3310f1eb9c00f738ec
Adrian Reyes <adrian_reyes1@yahoo.com>
2022-03-28 20:53:24 -0700

    testing with board.js

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,14 +1,12 @@
+import Container from "react-bootstrap/Container";
 
-import Container from 'react-bootstrap/Container'
-
- function Home() {
-
-    return (
-        <div>
-          <h1>hello world</h1>
-          <h2>this is home</h2>
-        </div>
-      );
-    }
+function Home() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <h2>this is home</h2>
+    </div>
+  );
+}
 
 export default Home;
--- client/src/components/library.js
+++ client/src/components/library.js
@@ -3,34 +3,32 @@ import { useEffect, useState } from "react";
 function Library() {
   const [data, setData] = useState([]);
   useEffect(() => {
-    console.log("hi");
     fetch("/library")
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         console.log(data);
         setData(data);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log("warn error");
       });
   }, []);
 
-  let logosFormatted = data.map((logo) => {
+  let logosFormatted = data.map(logo => {
     return (
-      <div>
-        <li key={logo.id}>{logo.company}</li>
+      <div key={logo.company}>
+        <li>{logo.company}</li>
       </div>
     );
   });
 
   return (
-    <div className="App" key={Library}>
+    <div key={Library}>
       <h1>hello world</h1>
       <h2>this is library</h2>
       <div>{logosFormatted}</div>
     </div>
   );
 }
-
 
 export default Library;
