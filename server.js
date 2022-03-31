f27adbba0bae35e7e575c6f93ebcc6c7ba9e203b
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2022-03-30 19:06:51 -0700

    deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
